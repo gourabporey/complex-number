c5588092d38d1d3ae248fea37094d39f02c3baa3
Gourab Porey <gourabporey@users.noreply.github.com>
2023-05-09 16:45:57 +0530

    Refactored `ComplexNumber` to use classes of Real and Imaginary numbers

--- src/complex-number.js
+++ src/complex-number.js
@@ -1,3 +1,6 @@
+const { Real } = require("./real.js");
+const { Imaginary } = require("./imaginary.js");
+
 class ComplexNumber {
   #real;
   #imaginary;
@@ -10,40 +13,48 @@ class ComplexNumber {
   getImaginaryPart() {
     return this.#imaginary;
   };
-
+  
   getRealPart() {
     return this.#real;
   };
 
+  #isNegative(number) {
+    return number.toString().includes("-");
+  };
+
+  equals(other) {
+    return this.#real.equals(other.#real) && this.#imaginary.equals(other.#imaginary);
+  }
+
   toString() {
-    if(this.#imaginary === 0) { 
-      return `${this.#real}`;
+    if(this.#imaginary.equals(new Imaginary(new Real(0)))) { 
+      return `${this.#real.toString()}`;
     };
 
-    if(this.#real === 0) {
-      return `${this.#imaginary}i`;
+    if(this.#real.equals(new Real(0))) {
+      return `${this.#imaginary.toString()}`;
     };    
 
-    const operator = this.#imaginary > 0 ? "+" : "-";
+    const operator = this.#isNegative(this.#imaginary) ? "" : "+";
 
-    return `${this.#real} ${operator} ${Math.abs(this.#imaginary)}i`;
+    return `${this.#real.toString()}${operator}${this.#imaginary.toString()}`;
   };
 
   add(other) {
-    const real = this.#real + other.#real;
-    const imaginary = this.#imaginary + other.#imaginary;
+    const real = this.#real.add(other.#real);
+    const imaginary = this.#imaginary.add(other.#imaginary);
 
     return new ComplexNumber(real, imaginary);
   };
 
-  mul(other) {
+  multiply(other) {
     const r = this.#real;
     const i = this.#imaginary;
     const or = other.#real;
     const oi = other.#imaginary;
 
-    const real = (r * or) - (i * oi);
-    const imaginary = (i * or) + (r * oi);
+    const real = r.multiply(or).add(i.multiply(oi));
+    const imaginary = i.multiplyReal(or).add(oi.multiplyReal(r));
 
     return new ComplexNumber(real, imaginary);
   };
--- test/complex-number-test.js
+++ test/complex-number-test.js
@@ -1,67 +1,92 @@
 const { describe, it } = require("node:test");
-const { deepStrictEqual, strictEqual } = require("assert");
+const { deepStrictEqual, strictEqual, ...assert } = require("assert");
+
 const { ComplexNumber } = require("../src/complex-number.js");
+const { Real } = require("../src/real.js");
+const { Imaginary } = require("../src/imaginary.js");
 
 describe("complexNumber", function() {
   describe("getImaginaryPart()", function() {
     it("Should give the imaginary part of the complex number", function() {
-      const cn = new ComplexNumber(3, 2);
-      strictEqual(cn.getImaginaryPart(), 2);
+      const a = new Real(3);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+
+      const actual = c.getImaginaryPart();
+      const expected = new Imaginary(new Real(2));
+      assert.ok(actual.equals(expected));
     });
   });
 
   describe("getRealPart()", function() {
     it("Should give the real part of the complex number", function() {
-      const cn = new ComplexNumber(3, 2);
-      strictEqual(cn.getRealPart(), 3);
+      const a = new Real(3);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+
+      const actual = c.getRealPart();
+      const expected = new Real(3);
+      assert.ok(actual.equals(expected));
     });
   });
 
-  describe("Stringify()", function() {
+  describe("toString()", function() {
     it("Should give the complex number in the form a + bi", function() {
-      const cn = new ComplexNumber(3, 2);
-      strictEqual(cn.toString(), "3 + 2i");
+      const a = new Real(3);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+      strictEqual(c.toString(), "3+2i");
     });
 
     it("Should give the complex number in the form bi", function() {
-      const cn = new ComplexNumber(0, 2);
-      strictEqual(cn.toString(), "2i");
+      const a = new Real(0);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+      strictEqual(c.toString(), "2i");
     });
 
     it("Should give the complex number in the form a", function() {
-      const cn = new ComplexNumber(3, 0);
-      strictEqual(cn.toString(), "3");
+      const a = new Real(0);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+      strictEqual(c.toString(), "2i");
     });
 
     it("Should give the complex number in the form a - bi", function() {
-      const cn = new ComplexNumber(3, -2);
-      strictEqual(cn.toString(), "3 - 2i");
+      const a = new Real(0);
+      const b = new Imaginary(new Real(2));
+      const c = new ComplexNumber(a, b);
+      strictEqual(c.toString(), "2i");
     });
   });
 
   describe("add()", function() {
     it("Should give the sum of two complex number", function() {
-      const a = new ComplexNumber(2, 3);
-      const b = new ComplexNumber(3, 2);
+      const [ a1, b1 ] = [ new Real(0), new Imaginary(new Real(2)) ];
+      const [ a2, b2 ] = [ new Real(4), new Imaginary(new Real(4))];
+      const [ er, ei ] = [ new Real(4), new Imaginary(new Real(6))];
+      
+      const [ c1, c2 ] = [ new ComplexNumber(a1, b1), new ComplexNumber(a2, b2)];
 
-      const expected = new ComplexNumber(5, 5);
-      const actual = a.add(b); 
-
-      deepStrictEqual(actual.getRealPart(), expected.getRealPart());
-      deepStrictEqual(actual.getImaginaryPart(), expected.getImaginaryPart());
+      const expected = new ComplexNumber(er, ei);
+      const actual = c1.add(c2); 
+      
+      assert.ok(expected.equals(actual));
     });
   });
 
-  describe("mul()", function() {
+  describe("multiply()", function() {
     it("Should give the product of two complex number", function() {
-      const a = new ComplexNumber(2, 3);
-      const b = new ComplexNumber(3, 2);
+      const [ a1, b1 ] = [ new Real(0), new Imaginary(new Real(2)) ];
+      const [ a2, b2 ] = [ new Real(4), new Imaginary(new Real(4))];
+      const [ er, ei ] = [ new Real(-8), new Imaginary(new Real(8))];
+      
+      const [ c1, c2 ] = [ new ComplexNumber(a1, b1), new ComplexNumber(a2, b2)];
 
-      const actual = a.mul(b); 
-      const expected = new ComplexNumber(0, 13);
-
-      deepStrictEqual(actual.getRealPart(), expected.getRealPart());
-      deepStrictEqual(actual.getImaginaryPart(), expected.getImaginaryPart());
+      const expected = new ComplexNumber(er, ei);
+      const actual = c1.multiply(c2); 
+      
+      assert.ok(expected.equals(actual));
     });
   });
 });
